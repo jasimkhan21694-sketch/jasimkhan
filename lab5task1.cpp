3fe005b0173a8c7ad9eb99ee38080a5c5206ded2
unzilanayab <unzilanayab06@gmail.com>
2025-10-24 14:06:51 +0500

    lab5 arry implemented with bubble sort

---
+++ lab5task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 25, 7, 42, 18};
+    cout << "Array elements: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
