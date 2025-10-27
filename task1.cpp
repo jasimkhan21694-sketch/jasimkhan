c29bde8136e8757c5110d51ff598e9a15efc7e95
unzilanayab <unzilanayab06@gmail.com>
2025-10-27 14:01:16 +0500

    print name subject roll no

---
+++ task1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Name: Jasim Khan" << endl;
+    cout << "Registration Number: 12345" << endl;
+    cout << "Course Name: Computer Science" << endl;
+
+    return 0;
+}
+
